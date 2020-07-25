c892d8f1763ffe17062f19bdada6df346ead4f46
Jisoo <fyimbtmn@gmail.com>
2020-07-25 19:41:33 +0900

    Add a fabfile(fabric3) for deploy

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+from fabric.contrib.files import append, exists, sed
+from fabric.api import env, local, run
+import random
+
+REPO_URL="https://github.com/DoodlesOnMyFood/tdd_test.git"
+
+def deploy():
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    source_folder = site_folder + '/source'
+    _create_directory_structure_if_necessary(site_folder)
+    _get_latest_source(source_folder)
+    _update_settings(source_folder, env.host)
+    _update_virtualenv(source_folder)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
+
+def _create_directory_structure_if_necessary(site_folder):
+    for subfolder in ('database', 'static', 'virtualenv', 'source'):
+        run(f'mkdir -p {site_folder}/{subfolder}')
+
+def _get_latest_source(source_folder):
+    if exists(source_folder + '/.git'):
+        run("cd {} && git fetch".format(source_folder))
+    else:
+        run("git clone {} {}".format(REPO_URL, source_folder))
+
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run('cd {} && git reset --hard {}'.format(source_folder, current_commit))
+
+def _update_settings(source_folder, site_name):
+    settings_path = source_folder + '/superlists/settings.py'
+    sed(settings_path, 'DEBUG = True', "DEBUG = False")
+    sed(settings_path, 'ALLOWED_HOSTS =.+$', 'ALLOWED_HOSTS = [\"{}\"]'.format(site_name))
+    secret_key_file = source_folder + '/superlists/secret_key.py'
+    if not exists(secret_key_file):
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-=+'
+        key = ''.join(random.SystemRandom().choice(chars) for i in range(50))
+        append(secret_key_file, f'SECRET_KEY = \'{key}\'')
+    append(settings_path, 'from .secret_key import SECRET_KEY')        
+
+def _update_virtualenv(source_folder):
+    virtualenv_folder = source_folder + '/../virtualenv'
+    if not exists(virtualenv_folder + '/bin/pip'):
+        run('python3 -m venv {}'.format(virtualenv_folder))
+    run('{}/bin/pip install -r {}/requirements.txt'.format(virtualenv_folder, source_folder))
+
+def _update_static_files(source_folder):
+    run('cd {} && ../virtualenv/bin/python manage.py collectstatic --noinput'.format(source_folder))
+
+def _update_database(source_folder):
+    run(f'cd {source_folder} && ../virtualenv/bin/python manage.py migrate --noinput')
+
+
+
